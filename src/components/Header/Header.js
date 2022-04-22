49556c04d8fa9e8be888abaae47fe28456a0ee40
tanverrana <tanversobur@gmail.com>
2022-04-22 23:00:03 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,46 @@
+import { signOut } from '@firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import logo from "../../images/logo/logo.png";
+import "./Header.css";
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
+
         <header>
-            <h2>This is header.</h2>
+            <Navbar className="navbar-container" collapseOnSelect expand="lg" sticky='top' variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo} height={60} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        </Nav>
+                        <Nav>
+
+                            {user ?
+                                <button className="btn btn-warning" onClick={handleSignOut}>Signout</button>
+                                :
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     );
 };
