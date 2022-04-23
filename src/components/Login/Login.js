786a2123d45e5a1b972e363583754d0533a0d8ae
tanverrana <tanversobur@gmail.com>
2022-04-23 09:21:54 +0600

    email and password authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef("");
     const passwordRef = useRef("");
     const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate("/home");
+    }
 
     const handleSubmit = event => {
         event.preventDefault();
 
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password);
     }
 
     const navigateRegister = event => {
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,21 +1,38 @@
 import React from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import "./Register.css";
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate("/login");
     }
 
+    if (user) {
+        navigate("/home");
+    }
+
     const handleRegister = event => {
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className="register-form">
             <h2 className="text-center my-4">Please Register!</h2>
-            <form>
+            <form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder="Enter Your Name" />
                 <input type="email" name="email" id="" placeholder="Enter Your Email" required /><br />
                 <input type="password" name="password" id="" placeholder="Enter Your Password" required />
